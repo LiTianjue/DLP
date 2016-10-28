82fc52105006c4392f0085315acf99ac18b78962
LiTianjue <mhpmii@126.com>
2016-10-28 11:17:05 +0800

    add httpMatcher

--- bin/dlp-syslogd.py
+++ bin/dlp-syslogd.py
@@ -4,6 +4,7 @@
 import json
 import sys
 import socket
+import re
 
 
 print "开始"
@@ -85,6 +86,77 @@ class LogSender:
 class DataMatchTool:
 	protol = "undefine"
 	def __init__(self,rulefile,protol):
-		self.protol = prontRuleList()
+		self.protol = protol
+		with open(rulefile,'r') as f:
+			self.data = json.load(f)
 	
 
+class HttpMatcher(DataMatchTool):
+	uri_rule_list=[]
+	html_rule_list=[]
+	xml_rule_list=[]
+	def __init__(self,rulefile):
+		DataMatchTool.__init__(self,rulefile, "http")
+		uri_array = self.data["rules"]["uri"]
+		for m in uri_array:
+			uri_json= json.loads(json.dumps(m))
+			self.uri_rule_list.append(uri_json)
+		html_array = self.data["rules"]["html"]
+		for m in html_array:
+			html_json= json.loads(json.dumps(m))
+			self.html_rule_list.append(html_json)
+		xml_array = self.data["rules"]["xml"]
+		for m in xml_array:
+			xml_json= json.loads(json.dumps(m))
+			self.xml_rule_list.append(xml_json)
+
+	def printRuleList(self):
+		print "------------uri---------------------"
+		for i in range(len(self.uri_rule_list)):
+			print "rule ",i+1,"key:",self.uri_rule_list[i]["key"],"msg:",self.uri_rule_list[i]["msg"]
+		print "------------html---------------------"
+		for i in range(len(self.html_rule_list)):
+			print "rule ",i+1,"key:",self.html_rule_list[i]["key"],"msg:",self.html_rule_list[i]["msg"]
+		print "------------xml---------------------"
+		for i in range(len(self.xml_rule_list)):
+			print "rule ",i+1,"key:",self.xml_rule_list[i]["key"],"msg:",self.xml_rule_list[i]["msg"]
+
+	def matchURI(self,URI):
+		for i in range(len(self.uri_rule_list)):
+			if URI.find(self.uri_rule_list[i]["key"]) >= 0:
+				return self.uri_rule_list[i]
+	def matchHTML(self,htmlfile):
+		with open(htmlfile,'r') as f:
+			str = f.read()	
+			for i in range(len(self.html_rule_list)):
+				match = re.search(self.html_rule_list[i]["key"], str)
+				if match :
+					return self.html_rule_list
+	def matchXML(self,xmlfile):
+		with open(xmlfile,'r') as f:
+			str = f.read()	
+			for i in range(len(self.xml_rule_list)):
+				match = re.search(self.xml_rule_list[i]["key"], str)
+				if match :
+					return self.xml_rule_list
+
+#########################################################
+
+
+
+
+#########################################################
+#unix socket
+
+
+
+
+
+
+if __name__ == "__main__":
+	m_httpmatcher = HttpMatcher(http_config_file)
+	# m_httpmatcher.printRuleList()
+	print m_httpmatcher.matchURI("Userlogin")
+	print type(m_httpmatcher.matchURI("login"))
+
+	print m_httpmatcher.matchHTML("sslvpn.html")
